94d745f5a7a119a670f522d8438fa6e520a03a8a
Khadija Naqvi <khadijanaqvi000@gmail.com>
2024-11-16 00:02:06 +0500

    product-image-slider

---
+++ index.js
@@ -0,0 +1,13 @@
+function hover(newImg) {
+    var small1 = document.getElementById("big-img").src = "assets/first.webp";
+}
+function hover1(newImg) {
+    var small2 = document.getElementById("big-img").src = "assets/second.webp"
+}
+
+function hover2(newImg) {
+    var small3 = document.getElementById("big-img").src = "assets/third.webp"
+}
+function hover3(newImg) {
+    var small4 = document.getElementById("big-img").src = "assets/last.webp"
+}
